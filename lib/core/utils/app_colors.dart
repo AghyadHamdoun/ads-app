979b2e92288ec7fcf7b165d816117196b5e474fa
AghyadHamdoun <100805550+AghyadHamdoun@users.noreply.github.com>
2023-05-29 14:55:37 +0300

    installing packages

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const primaryColor = Color(0xff364e8c);
+
+  static const accentColor =  Color(0xffccab79);
+
+  static const whiteColor =  Color(0xffffffff);
+
+  static const textColor =  Color(0xff333333);
+
+  static const hintText= Color(0xffA8A8A8);
+
+
+
+
+
+
+
+}
---
+++ lib/features/auth/presentation/screens/login.dart
@@ -0,0 +1,188 @@
+import 'package:ads/core/utils/app_colors.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  GlobalKey formKey = GlobalKey<FormState>();
+
+  TextEditingController emailController = TextEditingController();
+
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 0.3.sh,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 40.h),
+              height: 0.7.sh,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(45.r),
+                      topLeft: Radius.circular(45.r)),
+                  color: AppColor.primaryColor),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Welcome Back".tr(),
+                      style: TextStyle(
+                          fontSize: 26.sp,
+                          color: AppColor.whiteColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 40.h,
+                    ),
+                    Text(
+                      "Email:".tr(),
+                      style: TextStyle(
+                          fontSize: 16.sp,
+                          color: AppColor.whiteColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 4.h),
+                      height: 50.h,
+                      child: TextFormField(
+                        style: TextStyle(
+                            fontSize: 16.sp, color: AppColor.whiteColor),
+                        controller: emailController,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(
+                              top: 5.h, bottom: 10.h, right: 5.w, left: 5.w),
+                          hintText: "enter your email".tr(),
+                          hintStyle: TextStyle(
+                            color: AppColor.hintText,
+                            fontSize: 14.sp,
+                          ),
+                          icon: Icon(
+                            Icons.email,
+                            size: 20.r,
+                            color: AppColor.whiteColor,
+                          ),
+                          focusedBorder: const OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: AppColor.whiteColor)),
+                          enabledBorder: const OutlineInputBorder(
+                              borderSide: BorderSide(color: AppColor.hintText)),
+                          disabledBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(color: AppColor.hintText),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return tr(
+                              'Enter a valid email'.tr(),
+                            );
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                    Text(
+                      "password:".tr(),
+                      style: TextStyle(
+                          fontSize: 16.sp,
+                          color: AppColor.whiteColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 4.h),
+                height: 50.h,
+                child: TextFormField(
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  style: TextStyle(
+                      fontSize: 16.sp,
+                      color: AppColor.whiteColor),
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(
+                        top: 5.h, bottom: 10.h, right: 5.w, left: 5.w),
+                    hintText: "Password".tr(),
+                    hintStyle: TextStyle(
+                      color: AppColor.hintText,
+                      fontSize: 14.sp,
+                    ),
+                    icon: Icon(
+                      Icons.lock,
+                      size: 18.w,
+                      color: AppColor.whiteColor,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: AppColor.whiteColor)),
+                    enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: AppColor.whiteColor)),
+                    disabledBorder: const OutlineInputBorder(
+                      borderSide:
+                      BorderSide(color: AppColor.whiteColor),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return tr('Enter a password');
+                    }
+                    if (value.length < 8) {
+                      return tr('Password is short');
+                    }
+                    return null;
+                  },
+                ),
+              ),
+                    SizedBox(height: 20.h,),
+                    Align(
+                      alignment: Alignment.center,
+                      child: InkWell(
+                        onTap: (){},
+                        child: Container(height: 50.h,width:200.w,alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: AppColor.whiteColor,
+                          borderRadius: BorderRadius.circular(15.r)
+                        ),
+                        child: Text('login'.tr(),style: TextStyle(
+                          fontSize: 18.sp,
+                          color: AppColor.primaryColor,
+                          fontWeight: FontWeight.bold
+                        ),),),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}

1fb9dd37b1af60d93658315d5e033b43a28d411d
AghyadHamdoun <100805550+AghyadHamdoun@users.noreply.github.com>
2023-06-01 18:47:59 +0300

    profile screen

--- lib/features/pages/presentation/screens/profile.dart
+++ lib/features/pages/presentation/screens/profile.dart
@@ -1,4 +1,5 @@
 import 'package:ads/core/utils/app_colors.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -7,41 +8,91 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColor.primaryColor,
-      ),
+    return Scaffold(
       body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(vertical: 20.h),
-          child:Column(
-            children: [
-              Container(margin: EdgeInsets.symmetric(vertical: 10.h),height: 0.1.sh,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                shape: BoxShape.circle
-              ),
-              ),
-              Text("Aghyad",style: TextStyle(
-                color: Colors.black,
-                fontSize: 24.sp,
-                fontWeight: FontWeight.bold
-              ),),
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
+        child: Column(
+          children: [
+            Container(
+              color: AppColor.primaryColor,
+              padding: EdgeInsets.symmetric(horizontal: 10.w),
+              height: 0.25.sh,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Icon(Icons.star,color: Colors.amber,size: 34.r,),
-                  Icon(Icons.star,color: Colors.amber,size: 34.r,),
-                  Icon(Icons.star,color: Colors.amber,size: 34.r,),
-                  Icon(Icons.star,color: Colors.amber,size: 34.r,),
-                  Icon(Icons.star,color: Colors.amber,size: 34.r,),
+                  Container(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.h, horizontal: 10.w),
+                    height: 0.08.sh,
+                    width: 0.08.sh,
+                    decoration: BoxDecoration(
+                        color: Colors.black, shape: BoxShape.circle),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "Ahmed mohsen",
+                        style: TextStyle(
+                            color: AppColor.white,
+                            fontSize: 20.sp,
+                            fontWeight: FontWeight.w400),
+                      ),
+                      SizedBox(
+                        height: 5.h,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(2)),
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.symmetric(
+                            vertical: 2.h, horizontal: 5.w),
+                        child: Text(
+                          "Bronze",
+                          style:
+                              TextStyle(color: AppColor.white, fontSize: 14.sp),
+                        ),
+                      )
+                    ],
+                  ),
                 ],
-              )
-            ],
-          )
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 20.h),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.chat,
+                        size: 25.r,
+                        color: AppColor.colorExtended,
+                      ),
+                      SizedBox(
+                        width: 10.w,
+                      ),
+                      Text(
+                        "chats".tr(),
+                        style: TextStyle(
+                            color: AppColor.colorExtended, fontSize: 18.sp),
+                        textAlign: TextAlign.center,
+                      ),
+                      Spacer(),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        size: 20.r,
+                        color: AppColor.colorExtended,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
-    ));
+    );
   }
 }
